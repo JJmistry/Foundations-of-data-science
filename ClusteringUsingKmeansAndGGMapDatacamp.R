f394629e5d67a32f24dfc0953393afe55ac6ed2a
JJmistry <jayminm22@gmail.com>
2018-05-27 20:39:29 +0100

    Add files via upload

---
+++ ClusteringUsingKmeansAndGGMapDatacamp.R
@@ -0,0 +1,48 @@
+library(tidyverse)
+library(lubridate)
+library(VIM)
+library(ggmap)
+
+
+#Download data
+apr14 <- read.csv("https://raw.githubusercontent.com/fivethirtyeight/uber-tlc-foil-response/master/uber-trip-data/uber-raw-data-apr14.csv")
+may14 <- read.csv("https://raw.githubusercontent.com/fivethirtyeight/uber-tlc-foil-response/master/uber-trip-data/uber-raw-data-may14.csv")
+jun14 <- read.csv("https://raw.githubusercontent.com/fivethirtyeight/uber-tlc-foil-response/master/uber-trip-data/uber-raw-data-jun14.csv")
+jul14 <- read.csv("https://raw.githubusercontent.com/fivethirtyeight/uber-tlc-foil-response/master/uber-trip-data/uber-raw-data-jul14.csv")
+aug14 <- read.csv("https://raw.githubusercontent.com/fivethirtyeight/uber-tlc-foil-response/master/uber-trip-data/uber-raw-data-aug14.csv")
+sep14 <- read.csv("https://raw.githubusercontent.com/fivethirtyeight/uber-tlc-foil-response/master/uber-trip-data/uber-raw-data-sep14.csv")
+
+#bind into one dataframe
+data14 <- bind_rows(apr14, may14, jun14, jul14, aug14, sep14)
+
+#Examine structure of data
+summary(data14)
+str(data14)
+
+#Examine data for missing values
+aggr(data14)
+#No data missing
+
+#Format data to an appropriate format
+data14$Date.Time <- mdy_hms(data14$Date.Time)
+data14$Year <- factor(year(data14$Date.Time))
+data14$Month <- factor(month(data14$Date.Time))
+data14$Day <- factor(day(data14$Date.Time))
+data14$Weekday <- factor(wday(data14$Date.Time))
+data14$Hour <- factor(hour(data14$Date.Time))
+data14$Minute <- factor(minute(data14$Date.Time))
+data14$Second <- factor(second(data14$Date.Time))
+
+#Cluster data to identify Boroughs based on coordinates (We know there are 5 boroughs)
+clusters <- kmeans(data14[,2:3], 5)
+
+str(clusters)
+
+#Add in a Borough number to the dataset
+data14$Borough <- as.factor(clusters$cluster)
+
+#Plot on a ggmap
+
+NYCMap <- get_map("New York", zoom = 10)
+ggmap(NYCMap) + geom_point(aes(x = Lon[], y = Lat[], colour = as.factor(Borough)),data = data14) +
+  ggtitle("NYC Boroughs using KMean")
